811e72025403f8d08ed96a39bcb83510982a6bd7
Irene Brugnara <irene.brugnara@leader.it>
2021-02-07 19:31:50 +0100

    tested inputMove()

--- src/main/java/konobi/Position.java
+++ src/main/java/konobi/Position.java
@@ -1,5 +1,7 @@
 package konobi;
 
+import java.util.Objects;
+
 public class Position {
     private int x;
     private int y;
@@ -8,9 +10,24 @@ public class Position {
         this.x = x;
         this.y = y;
     }
-
+/*
     public boolean equals(Position otherPosition) {
         return this.x == otherPosition.x && this.y == otherPosition.y;
+    }
+    */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
     public static Position at(int x, int y){
--- src/test/java/konobi/testInputHandler.java
+++ src/test/java/konobi/testInputHandler.java
@@ -5,17 +5,30 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import static konobi.Position.at;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public class testInputHandler {
 
     @Test
-    public void YesAnswerIsReceived() {
+    public void yesAnswerIsReceived() {
         InputHandler inputhandler = new InputHandler();
         String input = "y";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         assertEquals(true, inputhandler.inputPie());
+
+    }
+
+    @Test
+    public void positionIsReceivedInInput() {
+        InputHandler inputhandler = new InputHandler();
+        String x = "0 0";
+        InputStream in = new ByteArrayInputStream(x.getBytes());
+        System.setIn(in);
+
+        assertEquals(at(0, 0), inputhandler.inputMove());
 
     }
 
